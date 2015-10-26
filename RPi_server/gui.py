0d49098db0b9c93466f68807ce4b5d4e4f1fbfb3
OSliusarenko <oleksii.sliusarenko@gmail.com>
2015-10-26 15:52:54 +0200

    some new sensor data

--- RPi_server/gui.py
+++ RPi_server/gui.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import ttk 
+import ttk
 import Tkinter as tk
 import tkMessageBox
 import threading
 
+import random # temporarily, remove after
+import string
+
 from functools import partial
 from time import sleep
 
+def randomString(length):
+    return ''.join(random.choice(string.lowercase+string.digits+
+                   string.uppercase) for i in range(length))
+
 class Gui(threading.Thread):
-    
+
     quitting = False
     commandsQueue = list()  # storage for all commands
-    
+
+
     def __init__(self):
         threading.Thread.__init__(self)
         self.start()
-        
+
+
     def initUI(self):
-      
+
         self.root.title("player")
         self.style = ttk.Style()
         self.style.theme_use("default")
@@ -30,15 +39,15 @@ class Gui(threading.Thread):
         self.root.rowconfigure(5, weight=1)
 
         self.lb = tk.Listbox(self.root)
-        self.lb.grid(row=0, column=0, columnspan=1, rowspan=6, 
+        self.lb.grid(row=0, column=0, columnspan=1, rowspan=6,
             padx=3, pady=3, sticky=tk.E+tk.W+tk.S+tk.N)
-            
-        self.lb.bind("<<ListboxSelect>>", self.onSelect)    
-            
-        ubtn = tk.Button(self.root, text=u"\u25b2", 
+
+        self.lb.bind("<<ListboxSelect>>", self.onSelect)
+
+        ubtn = tk.Button(self.root, text=u"\u25b2",
                          command=lambda : self.commandsQueue.append('u'))
         ubtn.grid(row=1, column=2)
-        
+
         dbtn = tk.Button(self.root, text=u"\u25bc",
                          command=lambda : self.commandsQueue.append('d'))
         dbtn.grid(row=3, column=2)
@@ -50,7 +59,7 @@ class Gui(threading.Thread):
         rbtn = tk.Button(self.root, text=u"\u25b6",
                          command=lambda : self.commandsQueue.append('r'))
         rbtn.grid(row=2, column=3, padx=5)
-        
+
         sbtn = tk.Button(self.root, text=u"\u25a0",
                          command=lambda : self.commandsQueue.append('s'))
         sbtn.grid(row=4, column=1)
@@ -61,65 +70,75 @@ class Gui(threading.Thread):
         pbtn.grid(row=4, column=2, columnspan=2)
 
         self.var = tk.StringVar()
-        self.label = tk.Label(self.root, text=0, textvariable=self.var)  
-        self.label.grid(row=6, column=0, columnspan=2, rowspan=1, 
+        self.label = tk.Label(self.root, text=0, textvariable=self.var)
+        self.label.grid(row=6, column=0, columnspan=2, rowspan=1,
             padx=1, pady=1, sticky=(tk.S+tk.W))
-            
+
         fbtn = tk.Button(self.root, text="flush", height=1, width=2,
                          command=self.flush)
         fbtn.grid(row=6, column=2)
         cbtn = tk.Button(self.root, text="Close", height=1, width=2,
                          command=partial(self.onExit, self.root))
         cbtn.grid(row=6, column=3)
-        
-#        self.root.pack(fill=tk.BOTH, expand=1)
 
 
     def flush(self):
-        
+        """ Temporary, delete afterwards """
         while self.commandsQueue != []:
             self.lb.insert(tk.END, self.commandsQueue.pop(0))
 
+
+    def feedToListbox(self, lines):
+        """ Displays specified list of strings on form's listbox """
+        for line in lines:
+            self.lb.insert(tk.END, line)
+
+
+    def clearListBox(self):
+        """ Just clears form's listbox """
+        self.lb.delete(0, tk.END)
+
+
     def onSelect(self, val):
-      
+
         sender = val.widget
         idx = sender.curselection()
-        value = sender.get(idx)   
+        value = sender.get(idx)
 
         self.var.set(value)
-        
+
+
     def onExit(self, frame):
-        """ Correctly destroys the window and says to main thread that 
+        """ Correctly destroys the window and says to main thread that
             we need to exit.
             Of course, since it is a member function of a class with
             just one window, we could use this function without 'frame'
-            argument, and just do 'self.root.quit()'. But it's fun to 
+            argument, and just do 'self.root.quit()'. But it's fun to
             use 'partial' :) """
         if tkMessageBox.askokcancel("Quit", "Stop playing?"):
             self.quitting = True
             frame.quit()
-        
+
+
     def run(self):
         self.root = tk.Tk()
-        self.root.protocol("WM_DELETE_WINDOW", 
+        self.root.protocol("WM_DELETE_WINDOW",
                                 partial(self.onExit, self.root))
 
         self.root.geometry("320x240+0+0")
 #        self.root.attributes("-fullscreen", True)
 
         self.initUI()
-        
+
         print 'Hello'
-        
+
         self.root.mainloop()
 
 
 
 def main():
-    
+
     gui = Gui()
-    
-    i = 0
 
     while True:
         if gui.quitting:
@@ -127,12 +146,19 @@ def main():
             break
         else:
             while gui.commandsQueue != []:
-                print gui.commandsQueue.pop(0)
-                
+                command = gui.commandsQueue.pop(0)
+                if command=='p':
+                    gui.clearListBox()
+                    msg = []
+                    for i in xrange(10):
+                        msg.append(randomString(15))
+                    gui.feedToListbox(msg)
+                else:
+                    print command
+
             sleep(0.1)
 
 
 
-
 if __name__ == '__main__':
-    main()  
+    main()
